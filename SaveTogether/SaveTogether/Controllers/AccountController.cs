53139410cdad15e31e9a46a0af3ac09b47b27bf6
tetiana.zhydeikina <tetiana.zhydeikina.kn.2016@lpnu.ua>
2017-02-25 04:24:23 -0800

    delete UserController and views for UserController add functional: delete user, edit user;

--- SaveTogether/SaveTogether/Controllers/AccountController.cs
+++ SaveTogether/SaveTogether/Controllers/AccountController.cs
@@ -71,14 +71,14 @@ namespace SaveTogether.Controllers
         {
             if (ModelState.IsValid)
             {
-                AuthorizedPerson customer = await UserManager.FindAsync(model.Email, model.Password);
-                if(customer == null)
+                AuthorizedPerson user = await UserManager.FindAsync(model.Email, model.Password);
+                if(user == null)
                 {
                     ModelState.AddModelError("", "Wrong login or password.");
                 }
                 else
                 {
-                    ClaimsIdentity claim = await UserManager.CreateIdentityAsync(customer, DefaultAuthenticationTypes.ApplicationCookie);
+                    ClaimsIdentity claim = await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
                     AuthenticationManager.SignOut();
                     AuthenticationManager.SignIn(new AuthenticationProperties
                     {
@@ -98,6 +98,58 @@ namespace SaveTogether.Controllers
         {
             AuthenticationManager.SignOut();
             return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirm()
+        {
+            AuthorizedPerson user = await UserManager.FindByEmailAsync(User.Identity.Name);
+            if(user != null)
+            {
+                IdentityResult result = await UserManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("LogOff", "Account");
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(EditModel model)
+        {
+            AuthorizedPerson user = await UserManager.FindByEmailAsync(User.Identity.Name);
+
+            if (user != null)
+            {
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.SecondName = model.SecondName;
+                user.DateOfBirth = model.DateOfBirth;
+
+                IdentityResult result = await UserManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Model has an errors.");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User is not found.");
+            }
+
+            return View(model);
         }
     }
 }
